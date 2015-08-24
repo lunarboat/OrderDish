4c45f597fe5a072f7119b2e9f33f8914fa09786a
lunarboat <lunarboat@outlook.com>
2015-08-24 08:37:58 +0800

    Initial Commit

---
+++ OrderDish/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OrderDish
+//
+//  Created by lunarboat on 15/8/24.
+//  Copyright (c) 2015年 lunarboat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
